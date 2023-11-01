790a77f66052a5b8f510774af711e2476878c178
vivekv2k3 <vivekv2k3@gmail.com>
2023-11-01 13:26:51 +0530

    c

--- login/src/App.js
+++ login/src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import './App.css'; // Import styles from App.css
+import './App.css';
 
 function CenteredText() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -13,9 +14,18 @@ function CenteredText() {
     setPassword(e.target.value);
   };
 
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleLogin = () => {
+    // Implement your login logic here
+    console.log("Logging in with email:", email, "and password:", password);
+  };
+
   return (
     <div className="centered-container">
-      <h1 className="centered-text">Doer</h1>
+      <h1 className="centered-text">Your Text Here</h1>
       <p className="login-text">Log In</p>
       <div className="input-container">
         <label className="input-label">Email</label>
@@ -28,13 +38,22 @@ function CenteredText() {
       </div>
       <div className="input-container">
         <label className="input-label">Password</label>
-        <input
-          type="password" // Use type="password for password input
-          value={password}
-          onChange={handlePasswordChange}
-          className="password-input"
-        />
+        <div className="password-input-container">
+          <input
+            type={showPassword ? "text" : "password"}
+            value={password}
+            onChange={handlePasswordChange}
+            className="password-input"
+          />
+          <input
+            type="checkbox"
+            onChange={togglePasswordVisibility}
+            checked={showPassword}
+            className="password-toggle"
+          />
+        </div>
       </div>
+      <button className="login-button" onClick={handleLogin}>Log In</button>
     </div>
   );
 }
